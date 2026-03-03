fd41bf08d05ff34eb9c5ca2efef7f52f9b64f215
apurva-code11 <lotiaapurva@gmail.com>
2026-03-03 16:18:59 +0530

    Add program to calculate square and cube of a number

---
+++ 09_Square_and_Cube.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Name: apurva \n");
+    printf("Square = %d\n", n * n);
+    printf("Cube = %d\n", n * n * n);
+    printf("Name:apurva \n");
+
+    return 0;
+}
