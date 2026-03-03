f87cefc6d840c278e854d046ae5ab1f63913aeae
apurva-code11 <lotiaapurva@gmail.com>
2026-03-03 16:21:03 +0530

    Create 11_Print_1_to_N.c

---
+++ 11_Print_1_to_N.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter N: ");
+    scanf("%d", &n);
+
+    printf("Name: apurva \n");
+
+    for (i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+
+    printf("\nName: apurva \n");
+
+    return 0;
+}
