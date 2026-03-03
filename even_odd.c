d5a547ed5cb2a493b245698ff327d3897a377591
apurva-code11 <lotiaapurva@gmail.com>
2026-03-03 16:08:05 +0530

    Create even_odd.c

---
+++ even_odd.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Name:apurva\n");
+
+    if (n % 2 == 0)
+        printf("Even\n");
+    else
+        printf("Odd\n");
+
+    printf("Name:apurva/n");
+
+    return 0;
+}
