18bb29a62f91ccbff9a7c60e163a6c51a9d3f526
apurva-code11 <lotiaapurva@gmail.com>
2026-03-03 16:03:20 +0530

    Add program to sum two numbers and display name

---
+++ 02_Add_Two_Numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    sum = a + b;
+
+    printf("Name: apurva\n");
+    printf("Sum = %d\n", sum);
+    printf("Name: apurva\n");
+
+    return 0;
+}
